a3476c59f94e71883af70bde73f8a42fbd467f16
kodai-kato <greatvoyage0122@gmail.com>
2020-03-13 18:19:31 +0900

    指摘点の微修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
   end
 
   it "名前が50文字以内であること" do
-    user = build(:user, name: "a"*51)
+    user = build(:user, name: "a" * 51)
     user.valid?
     expect(user.errors[:name]).to include("は50文字以内で入力してください")
   end
@@ -26,7 +26,7 @@ RSpec.describe User, type: :model do
   end
 
   it "メールアドレスが255文字以内であること" do
-    user = build(:user, email: "#{"a"*244}@example.com")
+    user = build(:user, email: "#{"a" * 244}@example.com")
     user.valid?
     expect(user.errors[:email]).to include("は255文字以内で入力してください")
   end
@@ -50,7 +50,7 @@ RSpec.describe User, type: :model do
   end
 
   it "パスワードが6文字以上であること" do
-    user = build(:user, password: "a"*6, password_confirmation: "a"*6)
+    user = build(:user, password: "a" * 6, password_confirmation: "a" * 6)
     user.valid?
     expect(user).to be_valid
   end
--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe "ユーザー登録", type: :request do
 
   it "有効なユーザーで登録" do
     expect {
-      post users_path, params: { user: { name:  "Example User",
+      post users_path, params: { user: { name: "Example User",
                                          email: "user@example.com",
-                                         password:              "password",
+                                         password: "password",
                                          password_confirmation: "password" } }
     }.to change(User, :count).by(1)
     redirect_to @user
@@ -23,9 +23,9 @@ RSpec.describe "ユーザー登録", type: :request do
 
   it "無効なユーザーで登録" do
     expect {
-      post users_path, params: { user: { name:  "",
+      post users_path, params: { user: { name: "",
                                          email: "user@example.com",
-                                         password:              "password",
+                                         password: "password",
                                          password_confirmation: "pass" } }
     }.not_to change(User, :count)
   end
