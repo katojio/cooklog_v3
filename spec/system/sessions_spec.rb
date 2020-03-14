5960f032e701f6091e7d4f2ecd0ebe386400a5d7
kodai-kato <greatvoyage0122@gmail.com>
2020-03-14 14:06:04 +0900

    ユーザー登録時にログインするように追記＆テスト追加

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -41,14 +41,14 @@ RSpec.describe "Sessions", type: :system do
       expect(page).to have_content 'メールアドレスとパスワードの組み合わせが誤っています'
 
       visit root_path
-      expect(page).to_not have_content "メールアドレスとパスワードの組み合わせが誤っています"
+      expect(page).not_to have_content "メールアドレスとパスワードの組み合わせが誤っています"
     end
 
     it "有効なユーザーでログインする前後でヘッダーが正しく表示されていることを確認" do
       expect(page).to have_link 'クックログとは？', href: about_path
       expect(page).to have_link 'ユーザー登録(無料)', href: signup_path
       expect(page).to have_link 'ログイン', href: login_path
-      expect(page).to_not have_link 'ログアウト', href: logout_path
+      expect(page).not_to have_link 'ログアウト', href: logout_path
 
       fill_in "user_email", with: user.email
       fill_in "user_password", with: user.password
@@ -58,7 +58,7 @@ RSpec.describe "Sessions", type: :system do
       expect(page).to have_link 'ユーザー一覧', href: users_path
       expect(page).to have_link 'プロフィール', href: user_path(user)
       expect(page).to have_link 'ログアウト', href: logout_path
-      expect(page).to_not have_link 'ログイン', href: login_path
+      expect(page).not_to have_link 'ログイン', href: login_path
     end
   end
 end
