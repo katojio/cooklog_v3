f20ddecd367dd7c7c9d8c004acfc5de00e9a832e
kodai-kato <greatvoyage0122@gmail.com>
2020-03-29 14:16:31 +0900

    ユーザー削除のテスト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,13 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    if current_user?(@user)
+    # 管理者ユーザーの場合
+    if current_user.admin?
+      @user.destroy
+      flash[:success] = "ユーザーの削除に成功しました"
+      redirect_to users_url
+    # 管理者ユーザーではないが、自分のアカウントの場合
+    elsif current_user?(@user)
       @user.destroy
       flash[:success] = "自分のアカウントを削除しました"
       redirect_to root_url
