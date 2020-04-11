14a6d1419264a5c25eeafa94db5ca7e3f3fe277c
kodai-kato <greatvoyage0122@gmail.com>
2020-04-11 22:50:34 +0900

    Dishコントローラにcorrect_userメソッドを追加

--- spec/requests/dishes_edit_spec.rb
+++ spec/requests/dishes_edit_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "料理編集", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
   let!(:dish) { create(:dish, user: user) }
 
   context "認可されたユーザーの場合" do
@@ -38,6 +39,26 @@ RSpec.describe "料理編集", type: :request do
                                                popularity: 5 } }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
+    end
+  end
+
+  context "別アカウントのユーザーの場合" do
+    it "ホーム画面にリダイレクトすること" do
+      # 編集
+      login_for_request(other_user)
+      get edit_dish_path(dish)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+      # 更新
+      patch dish_path(dish), params: { dish: { name: "イカの塩焼き",
+                                               description: "冬に食べたくなる、身体が温まる料理です",
+                                               portion: 1.5,
+                                               tips: "ピリッと辛めに味付けするのがオススメ",
+                                               reference: "https://cookpad.com/recipe/2798655",
+                                               required_time: 30,
+                                               popularity: 5 } }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
     end
   end
 end
